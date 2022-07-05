bab1ea5b22de404dc9978d9d21fbdd2b94d39e0c
Evgeniy Tkachenko <tkachenko.evgeniy@gmail.com>
2022-07-05 12:14:35 +0300

    HW-07 22-07-05

---
+++ src/telran/text/Strings.java
@@ -0,0 +1,171 @@
+package telran.text;
+
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.Arrays;
+
+
+public class Strings {
+/**
+ * 
+ * @param str1 ascii string with no repeated symbols
+ * @param str2 ascii string with no repeated symbols
+ * @return array with two numbers
+ *    first - number of the symbols of the str2 that exist in str1 at the same indexes
+ *    second - number of the symbols of the str2 that exist in str1 at different indexes
+ */
+	public static int[] deepNoRepeatedCompare(String str1, String str2) {
+	int Ares [] = {0, 0};
+	for(int i = 1; i <= str2.length(); i++) {
+		int pInd = str1.indexOf(str2.substring(i-1, i));
+		if ((pInd > - 1) && (pInd == i-1)) {
+			Ares[0]++;
+		      }
+			else {
+				if (pInd > - 1) Ares[1]++;
+			}
+	}
+
+		//Two implementation hints
+		//   first: additional helper array such that array['a'] 
+		//          is an index of the symbol 'a' in str1 or -1
+		//          example, str1 - "ctab" then array['a'] = 2;
+		// ************************************************
+		//   second: implementation based on the method indexOf
+
+	return Ares;
+}
+	/**
+	 * 
+	 * @param str1 English letters (may have repeats)
+	 * @param str2 English letters (may have repeats)
+	 * @return true if :
+	 *     (1) str2 has the same as str1 length
+	 *     (2) str2 comprises of all letters from str1
+	 */
+	public static boolean isAnagram(String str1, String str2) {
+
+			char[] ResStr1 = str1.toCharArray();		
+			char[] ResStr2 = str2.toCharArray();
+			
+			Arrays.sort(ResStr1);
+		 	Arrays.sort(ResStr2);
+		 	
+		// additional helper array such that array['a'] is the number of 'a' occurrences in str1
+		//str1 = "hello", array['l'] = 2; array['w'] = 0;
+		return   Arrays.equals(ResStr1, ResStr2);
+		
+	
+}
+	static public String join(String[] array, String delimiter) {
+		//STring "+" operator based solution
+		return stringPluseSolution(array, delimiter);
+		//StringBuilder attend based solution
+		//return stringBuilderSolution(array, delimiter);
+	}
+	static private String stringBuilderSolution(String[] array, String delimiter) {
+		StringBuilder strBuilder = new StringBuilder(array[0]);
+		for(int i = 1; i < array.length; i++) {
+			strBuilder.append(delimiter).append(array[i]);
+		}
+		return strBuilder.toString();
+	}
+	static private String stringPluseSolution(String[] array, String delimiter) {
+		String res = array[0]; //assumption: there is at least one string
+		for(int i = 1; i < array.length; i++) {
+			res += delimiter + array[i] ;
+		}
+		return res;
+	}
+	/**
+	 * 
+	 * @param name1 -  first name 
+	 * @param name2 - second name
+	 * @return either "match" or "no match" in accordance to the comments (see TODO)
+	 */
+	static public String matches(String name1, String name2) {
+		
+		if (name1.compareToIgnoreCase(name2) == 0) return "match";
+		
+		String res  = "0";
+        String[] nameA = name1.split(" ");
+        String[] nameB = name2.split(" "); 
+       
+        //sit1
+        if (nameA.length == 1 && nameB.length == 1) res  = "no match";
+        
+        //Sit2
+        if (nameA.length == 2 && nameB.length == 2) {
+        	if (nameA[0].compareToIgnoreCase(nameB[0]) == 0) {
+        		if (nameA[1].charAt(0) == nameB[1].charAt(0)) {
+        			res ="match";        			
+        		}
+        		else res = "no match";
+        	}
+        }
+        
+        //sit3
+        if (nameA.length == 3 && nameB.length == 2) {
+        	
+        	if (nameA[2].compareToIgnoreCase(nameB[1]) == 0) {
+        	
+        		if (nameA[1].equalsIgnoreCase(nameB[0]) || (nameA[0].equalsIgnoreCase(nameB[0]))){
+        			res = "match";
+        		}
+        		else res = "no match";
+        	}
+            	
+        }
+        
+        //sit4
+        if (nameA.length == 3 && nameB.length == 3) res  = "no match";
+  
+        
+//      String matches(String name1, String name2)
+//      Names match under the following conditions, after breaking each one into "name parts" on whitespace :
+//      - Two name parts match if they are the same (case insensitive) or one is a single letter initial and the other is longer but starts with the same letter.
+//      - A name part in  the name1 may be missing from the name2
+//      - Name parts in one name must not contradict name parts in the other
+//      - Name parts that match must be in the same order in both names
+		return res;
+	}
+	/**
+	 * sorts array of strings
+	 * @param strNumbers array of strings containing the positive integer numbers
+	 * length of each string can not be more than three symbols
+	 * String containing "123" should be greater than string containing "23" as the number 123 greater than
+	 * number 23
+	 */
+	static public int[] sortStringsAsNumbers(String[] strNumbers) {
+//Algorithm complexity should be O[N]
+//Implementation hint: additional helper array such that ar[10] - count of occurrences
+//of number 10 in the given array
+//find out how to get number from a string and how to convert number to string
+		int[] matrix;
+		matrix = new int[999]; 
+		int numStr = strNumbers.length;
+		int[] res;
+		res = new int[numStr];
+		int numMax = 0;
+		int numTek;
+		
+			for(int i = 0; i < numStr ; i++) {
+			numTek = Integer.parseInt(strNumbers[i]);
+			
+				if (numTek > numMax) numMax = numTek;
+				matrix[numTek]++;
+			}
+		int j = 0;
+		
+			for(int i = numMax; i > 0 ; i--) {
+				if (matrix[i] > 0) {
+						res[j] = i;
+						j++;
+				}
+			}
+				
+		return res;
+	}
+
+}
--- src/telran/text/test/StringsClassTests.java
+++ src/telran/text/test/StringsClassTests.java
@@ -5,9 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import telran.text.Stringstext;
+import telran.text.Strings;
 
 class StringsClassTests {
+	private static final int N_RUNS = 1;
+	private static final int N_STRINGS = 1;
 
 	@Test
 	void deepCompareTest() {
@@ -20,10 +22,10 @@ class StringsClassTests {
 		int expected2[] = {1, 1};
 		int expected3[] = {0, 8};
 		int expected4[] = {0, 0};
-		assertArrayEquals(expected1, Stringstext.deepNoRepeatedCompare(str1, str2));
-		assertArrayEquals(expected2, Stringstext.deepNoRepeatedCompare(str1, str3));
-		assertArrayEquals(expected3, Stringstext.deepNoRepeatedCompare(str1, str5));
-		assertArrayEquals(expected4, Stringstext.deepNoRepeatedCompare(str1, str4));
+		assertArrayEquals(expected1, Strings.deepNoRepeatedCompare(str1, str2));
+		assertArrayEquals(expected2, Strings.deepNoRepeatedCompare(str1, str3));
+		assertArrayEquals(expected3, Strings.deepNoRepeatedCompare(str1, str5));
+		assertArrayEquals(expected4, Strings.deepNoRepeatedCompare(str1, str4));
 	}
 	
 	@Test
@@ -32,10 +34,74 @@ class StringsClassTests {
 		String str2 = "Helll";
 		String str3 = "eloHl";
 		String str4 = "Helo";
-		assertTrue(Stringstext.isAnagram(str1, str3));
-		assertFalse(Stringstext.isAnagram(str1, str2));
-		assertFalse(Stringstext.isAnagram(str1, str4));
-		assertTrue(Stringstext.isAnagram(str1, str1));
+		assertTrue(Strings.isAnagram(str1, str3));
+		assertFalse(Strings.isAnagram(str1, str2));
+		assertFalse(Strings.isAnagram(str1, str4));
+		assertTrue(Strings.isAnagram(str1, str1));
 	}
+	@Test
+	void joinTest() {
+		String array[] = {"Hello", "Vasya"};
+		String expected = "Hello Vasya";
+		assertEquals(expected, Strings.join(array, " "));
+	}
+	@Test
+	void joinPerformanceTest() {
+		String array[] = getBigArray();
+		for(int i = 0; i < N_RUNS; i++) {
+			Strings.join(array, " ");
+		}
+	}
+
+	private String[] getBigArray() {
+		String res[] = new String[N_STRINGS];
+		for (int i = 0; i < res.length; i++) {
+			res[i] = "Hello";
+		}
+		return res;
+	}
+	@Test
+	void matchesTest() {
+		//TODO
+		//        Examples:
+//	        - David vs. david: match
+//	        - John F vs. John Fitzgerald: match
+//	        - John K vs. John Fitzgerald: no match
+//	        - Anna Maria Magdalena vs. Anna Magdalena: match
+//	        - Anna Maria Magdalena vs. Maria Magdalena: match
+//	        - Anna Maria Magdalena vs. Anna Maria Roberta: no match
+//	        - Anna Maria Magdalena vs. Anna Magdalena Roberta: no match
+//	        - Anna Maria Magdalena vs. Anna Magdalena Maria: no match
+		String str1 = "David";
+		String str2 = "david";
+		String str3 = "John F";
+		String str4 = "John Fitzgerald";
+		String str5 = "John K";
+		String str6 = "Anna Maria Magdalena";
+		String str7 = "Anna Magdalena";
+		String str8 = "Maria Magdalena";
+		String str9 = "Anna Maria Roberta";
+		String str10 = "Anna Magdalena Roberta";
+		String str11 = "Anna Magdalena Maria";
+		String expectedY = "match";
+		String expectedN = "no match";
+		assertEquals(expectedY, Strings.matches(str1, str2));   
+		assertEquals(expectedY, Strings.matches(str3, str4)); 
+		assertEquals(expectedN, Strings.matches(str5, str4)); 
+		assertEquals(expectedY, Strings.matches(str6, str7)); 
+		assertEquals(expectedY, Strings.matches(str6, str8)); 
+		assertEquals(expectedN, Strings.matches(str6, str9)); 
+		assertEquals(expectedN, Strings.matches(str6, str10)); 
+		assertEquals(expectedN, Strings.matches(str6, str11)); 
+			
+	}
+	@Test
+	void sortStringsAsNumbersTest() {
+		String str[] = {"123", "254", "23", "547", "11"};
+		int expected[] = {547, 254, 123, 23, 11};
+		
+		assertArrayEquals(expected, Strings.sortStringsAsNumbers(str));
+	}
+
 
 }
